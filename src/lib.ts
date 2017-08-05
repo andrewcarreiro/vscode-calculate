0e4f3ba9896b89376d9b48c460c6ce39143dc160
Andrew Carreiro <andrew@headfirst.ca>
2017-08-05 12:39:39 -0400

    wip

--- src/lib.ts
+++ src/lib.ts
@@ -1,14 +1,68 @@
 
 export function indexToAlpha ( index : number ) {
 	let currentIndex = index;
-	let currentOutputString = "";
-	const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const BASE = alphabet.length;
+  let places = 0;
+  let output = [];
 
-	do {
-		let chosenAlpha = alphabet[currentIndex % alphabet.length];
-		currentOutputString = chosenAlpha + currentOutputString;
-		currentIndex -= alphabet.length;
-	} while (currentIndex >= 0 );
+  if( index < 26 ){
+    return alphabet[index];
+  }
 
-	return currentOutputString;
+  // find number of positions in index when converted to base 25
+  let tmpNumber = 0;
+  tmpNumber = Math.pow(BASE,places);
+  while( tmpNumber < index ){
+    tmpNumber = Math.pow(BASE,places);
+    places ++;
+  }
+
+  // console.log(`places: ${places} in ${index}`);
+
+  // work our way back from the big number, assigning values as we go
+  for( let i=places; i>=0; i--){
+    const column = Math.pow(BASE,i);
+    const chosenValue = Math.floor(currentIndex / column );
+    // console.log(`col: ${column}, chosenValue ${index}`);
+    output.push(chosenValue);
+    currentIndex -= chosenValue;
+  }
+
+  // output.shift();
+  return output.map( v => alphabet[v]).join("");
+
+
+  // so I'm not converting to 
+  // 
+
+
+	// do {
+  //   // console.log('current index: '+currentIndex);
+  //   let chosenAlpha = alphabet[currentIndex % alphabet.length];
+  //   console.log(index+': chosen alpha - '+chosenAlpha+' - currentindex - '+currentIndex);
+	// 	currentOutputString = chosenAlpha + currentOutputString;
+	// 	currentIndex -= alphabet.length;
+	// } while (currentIndex >= 0 );
+
+	// return output;
 }
+
+/**
+  5
+        5
+  105
+  1 0 5
+
+  54
+  0 5  4
+
+  284
+  10^2 10^1 10^0
+  100  10   1
+  2    8    4
+
+
+  0005
+  aaaf
+**/
--- test/lib_test.ts
+++ test/lib_test.ts
@@ -5,6 +5,10 @@ describe("lib", function () {
 	describe("indexToAlpha", function() {
 		it("should convert 0 to a", function () {
 			expect(lib.indexToAlpha(0)).toBe("a")
+    });
+    
+    it("should convert 25 to z", function () {
+			expect(lib.indexToAlpha(25)).toBe("z")
 		});
 		
 		it("should convert 26 to aa", function () {
@@ -13,6 +17,19 @@ describe("lib", function () {
 		
 		it("should convert 27 to ab", function () {
 			expect(lib.indexToAlpha(27)).toBe("ab")
-		});
+    });
+    
+    it("should convert 28 to ac", function () {
+      expect(lib.indexToAlpha(28)).toBe("ac")
+    });
+    
+    it("should never generate an identical record", function () {
+      let arr = [];
+      for( let j=0; j<200000; j++){
+        arr.push(j);
+      }
+      arr = arr.map( ( v, i ) => lib.indexToAlpha(i) );
+      expect(Array.from(new Set(arr)).length).toBe(arr.length)
+    });
 	});
 });
